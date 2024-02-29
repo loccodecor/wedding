7f6efe32d554e4b329716fee8fa509a29312c64f
loccodecor <loccodecor@gmail.com>
2024-02-28 18:13:37 -0800

    Success form!!!!!!!

--- js/scripts.js
+++ js/scripts.js
@@ -213,12 +213,7 @@ $(document).ready(function () {
         var data = $(this).serialize();
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbygmrwyxSM_iQdn5TKzb8ncRIHWmGaUG_YEIrowic3WwTIEe9zHpWH-5f9jXOowBgtA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxzcKUbJwuxikXjfxklvQXf0CY4AuBFE8qrW4ELhdwvtwCRoRlAYUlGabzMOPR4hrg/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -232,7 +227,7 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
-        }
+        
     });
 
 });
